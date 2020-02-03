dbbd146174e0231ef37e5297b0111666992f0750
Prashant Vishwakarma <prashant.vishwakarma@news.co.uk>
2020-02-03 23:29:24 +0530

    Added groovy, grails installation and refactored the script

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
 # automate_mac_setup
-Autoate Mac setup for a new Mac system.
+Automated setup for a new Mac system.
 
-Before runnung script you need to provide executable permission to the scrip.
-Use below command in terminal to make to executable.
+-- Steps to execute the script --
+
+1. make script executable.
 
 chmod u+x setup_my_mac_script_1.sh
 
-To run the script 
+2. Run the script
 
 ./setup_my_mac_script_1.sh
 
+--------------------------------------
+
+This script will install and do below mentioned things.
+
+1. check **"~/.bash_profile"** if not present it will create one. 
+2. **BREW** (Homebrew) Mac package management tool.
+3. **Java v8** (AdoptOpenJDK8) also set JAVA_HOME into PATH variable.
+4. **GRADLE** dependency management tool for Java library and projects.
+5. **MAVEN** dependency management tool for Java library and projects.
+6. **SDKMAN**, An additional Mac package management tool.
+7. **GROOVY**
+8. **GRAILS**
+9. **GIT**
+10. **DOCKER**
--- setup_my_mac_script_1.sh
+++ setup_my_mac_script_1.sh
@@ -1,76 +1,107 @@
 #!/bin/sh
 
-echo "\n\n=== Automated Mac Setup v0.1 ==="
+echo "\n\n=== Automated Mac Setup v0.2 ==="
+echo "\n\n\nChecking '.bash_profile' \n\n"
 
-echo "\n\nBREW (HOMEBREW) Mac Package Manager\n\n"
-
-if [ -n `which brew` ]; then
-        echo "BREW is already installed\n"
-        brew_version=$((brew -v) 2>&1)
-        echo "$brew_version \n"
+if [ -r ~/.bash_profile ]; then
+  echo "'.bash_profile' exists"
 else
-        echo "Installing BREW\n"
+  echo "'.bash_profile' does not exists\n"
+  touch ~/.bash_profile
+  echo 'export PATH=/usr/local/bin:$PATH' >>~/.bash_profile
+  chmod 700 ~/.bash_profile
+  echo "'.bash_profile' is created"
+fi
+
+echo "\n\n\nBREW (HOMEBREW) Mac Package Manager\n\n"
+if [ -n `which brew` ]; then
+  echo "BREW is already installed"
+else
+  echo "Installing BREW"
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+brew -v
 
-echo "\n\nJAVA AdoptOpenJDK8\n\n"
-
+echo "\n\n\nJAVA AdoptOpenJDK8\n\n"
 if [ -n `which java` ]; then
-	echo "JAVA is already installed\n"
-	java_version=$((java -version) 2>&1)
-	echo "$java_version \n"
+	echo "JAVA is already installed"
 else
-	echo "Installing JAVA AdoptOpenJDK8 \n"
+	echo "Installing JAVA AdoptOpenJDK8"
 	brew tap AdoptOpenJDK/openjdk
 	brew cask install adoptopenjdk8
-	java -version
+	echo 'export JAVA_HOME=`/usr/libexec/java_home`' >>~/.bash_profile
+  echo 'export PATH=$PATH:$JAVA_HOME' >>~/.bash_profile
 fi
+java -version
 
-echo "\n\nGRADLE Package Management\n\n"
 
+echo "\n\n\nGRADLE Package Management\n\n"
 if [ -n `which gradle` ]; then
-        echo "GRADLE is already installed\n"
-        gradle_version=$((gradle -v) 2>&1)
-        echo "$gradle_version \n"
+  echo "GRADLE is already installed"
 else
-        echo "Installing GRADLE \n"
-        brew install gradle
+  echo "Installing GRADLE"
+  brew install gradle
 fi
+gradle -v
 
-echo "\n\nMAVEN Package Management\n\n"
+echo "\n\n\nMAVEN Package Management\n\n"
+if [ -n `which mvn` ]; then
+  echo "MAVEN is already installed"
+else
+  echo "Installing MAVEN"
+  brew install maven
+fi
+mvn -v
 
-	brew install maven
-	mvn -v
-
-echo "\n\nSDKMAN Mac Package Management (Extra)\n\n"
-
+echo "\n\n\nSDKMAN Mac Package Management (Extra)\n\n"
+if [ -d "$SDKMAN_DIR" ]; then
+	echo "SDKMAN is already installed"
+else
+  echo "Installing SDKMAN"
 	curl -s "https://get.sdkman.io" | bash
-
-echo "\n\nGIT Version Management\n\n"
-
-if [ -n `which git` ]; then
-        echo "GIT is already installed\n"
-        git_version=$((git version) 2>&1)
-        echo "$git_version \n"
-else
-        echo "Installing GIT \n"
-        brew install git
 fi
+sdk version
 
-echo "\n\nDOCKER Container Engine\n\n"
+echo "\n\n\n\Checking GROOVY \n\n"
+if [ -n `which groovy` ]; then
+  echo "GROOVY is already installed"
+else
+  echo "Installing GROOVY"
+  sdk install groovy
+fi
+groovy -v
+
+echo "\n\n\n\Checking GRAILS \n\n"
+if [ -n `which grails` ]; then
+  echo "GRAILS is already installed"
+else
+  echo "Installing GRAILS"
+  sdk install grails
+fi
+grails -version
+
+echo "\n\n\nGIT Version Management\n\n"
+if [ -n `which git` ]; then
+  echo "GIT is already installed"
+else
+  echo "Installing GIT"
+  brew install git
+fi
+git version
+
+echo "\n\n\nDOCKER Container Engine\n\n"
 
 if [ -n `which docker` ]; then
-        echo "DOCKER is already installed\n"
-        docker_version=$((docker -v) 2>&1)
-        echo "$docker_version \n"
+  echo "DOCKER is already installed\n"
 else
-        echo "Installing DOCKER \n"
-        brew tap phinze/homebrew-cask
+  echo "Installing DOCKER \n"
+  brew tap phinze/homebrew-cask
 	brew install brew-cask
 	brew cask install docker
-	open /Applications/Docker.app
-	docker -v
 fi
+docker -v
+open /Applications/Docker.app
+
+source ~/.bash_profile
 
 echo "=== Completed ==="
-echo "== RUN THE NEXT SCRIPT =="
