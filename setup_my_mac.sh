6fab7044f992c66c324189eff621becb9bb54aa5
prashant-nts <56384996+prashant-nts@users.noreply.github.com>
2020-02-07 16:58:24 +0530

    Fix script

--- setup_my_mac.sh
+++ setup_my_mac.sh
@@ -7,44 +7,62 @@ if [ ! -r ~/.bash_profile ]; then
   touch ~/.bash_profile
   echo 'export PATH=/usr/local/bin:$PATH' >>~/.bash_profile
   chmod 700 ~/.bash_profile
+  task_done+=(.bash_profile---build)
 fi
-task_done+=(.bash_profile---build)
-if [ ! -n `which brew` ]; then
+if [ -n `which brew` ]; then
+	echo ""
+else
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	brew tap caskroom/cask
+	task_done+=(Homebrew----installed)
 fi
-task_done+=(Homebrew----installed)
-if [ ! -n `which java` ]; then
+if [ -n `which java` ]; then
+	echo ""
+else
 	brew tap AdoptOpenJDK/openjdk
 	brew cask install adoptopenjdk8
 	echo 'export JAVA_HOME=`/usr/libexec/java_home`' >>~/.bash_profile
   echo 'export PATH=$PATH:$JAVA_HOME' >>~/.bash_profile
+  task_done+=(OpenJDK8----installed)
 fi
-task_done+=(OpenJDK8----installed)
-if [ ! -n `which gradle` ]; then
+if [ -n `which gradle` ]; then
+	echo ""
+else
   brew install gradle
+  task_done+=(Gradle------installed)
 fi
-task_done+=(Gradle------installed)
-if [ ! -n `which mvn` ]; then
+if [ -n `which mvn` ]; then
+	echo ""
+else
   brew install maven
+  task_done+=(Maven-------installed)
 fi
-task_done+=(Maven-------installed)
+
 if [ ! -d "$SDKMAN_DIR" ]; then
 	curl -s "https://get.sdkman.io" | bash
+	task_done+=(SdkMan------installed)
 fi
-task_done+=(SdkMan------installed)
-if [ ! -n `which groovy` ]; then
+
+if [ -n `which groovy` ]; then
+	echo ""
+else
   sdk install groovy
+  task_done+=(Groovy------installed)
 fi
-task_done+=(Groovy------installed)
-if [ ! -n `which grails` ]; then
+
+if [ -n `which grails` ]; then
+	echo ""
+else
   sdk install grails
+  task_done+=(Grails------installed)
 fi
-task_done+=(Grails------installed)
-if [ ! -n `which git` ]; then
+
+if [ -n `which git` ]; then
+	echo ""
+else
   brew install git
+  task_done+=(Git---------installed)
 fi
-task_done+=(Git---------installed)
 brew cask install postman
 task_done+=(Postman-----installed)
 brew cask install sourcetree
@@ -63,12 +81,14 @@ echo "alias ll='ls -ltr -a'" >>~/.bash_profile
 echo "parse_git_branch() { git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' }" >>~/.bash_profile
 echo 'export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "' >>~/.bash_profile
 
-if [ ! -n `which docker` ]; then
-  brew tap phinze/homebrew-cask
+if [ -n `which docker` ]; then
+	echo ""
+else
+  	brew tap phinze/homebrew-cask
 	brew install brew-cask
 	brew cask install docker
+	task_done+=(Docker-------installed)
 fi
-task_done+=(Docker-------installed)
 for i in ${task_done[@]}
 do
   echo $i
